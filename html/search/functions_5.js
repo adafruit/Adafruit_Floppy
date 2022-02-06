af95b7fa0d7743e211e15a8299eaae4d5e974466
Doxygen CI <ci-arduino@invalid>
2022-02-06 03:58:31 +0000

    Deploy docs to GitHub Pages from commit c747af3a87
    Commit: c747af3a872ec9c53200bf532c151d9e52ec7b61
    GitHub Actions run: 1801284609

--- html/search/functions_5.js
+++ html/search/functions_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['print_5fpulse_5fbins',['print_pulse_bins',['../class_adafruit___floppy.html#ae689c8679faffd92690e08aedaf23d73',1,'Adafruit_Floppy']]],
-  ['print_5fpulses',['print_pulses',['../class_adafruit___floppy.html#a40692a5a89a3d4292937477bd17a2ed7',1,'Adafruit_Floppy']]]
+  ['print_5fpulses',['print_pulses',['../class_adafruit___floppy.html#ad71ee07c030519cb0f0d8cc937df91fc',1,'Adafruit_Floppy']]]
 ];
