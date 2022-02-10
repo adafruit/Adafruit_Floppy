311c7798fbf75b72d17b78c40bdbf79f1ac053a6
Jeff Epler <jepler@gmail.com>
2022-02-10 09:49:57 -0600

    GW firmware is unreliable unless Adafruit TinyUSB is used

--- examples/greaseweazle/greaseweazle.ino
+++ examples/greaseweazle/greaseweazle.ino
@@ -28,6 +28,9 @@
   #define READ_PIN      9    // IDC 30
   #define SIDE_PIN      8    // IDC 32
   #define READY_PIN     7    // IDC 34
+#ifndef USE_TINYUSB
+#error "Please set Adafruit TinyUSB under Tools > USB Stack"
+#endif
 #elif defined (ARDUINO_RASPBERRY_PI_PICO)
   #define DENSITY_PIN  2     // IDC 2
   #define INDEX_PIN    3     // IDC 8
@@ -42,6 +45,9 @@
   #define READ_PIN    12     // IDC 30
   #define SIDE_PIN    13     // IDC 32
   #define READY_PIN   14     // IDC 34
+#ifndef USE_TINYUSB
+#error "Please set Adafruit TinyUSB under Tools > USB Stack"
+#endif
 #else
 #error "Please set up pin definitions!"
 #endif
