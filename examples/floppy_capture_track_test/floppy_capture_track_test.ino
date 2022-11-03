64e9db7cc52287e9662406bb944e5ec1e0607531
Jeff Epler <jepler@gmail.com>
2022-11-03 08:56:06 -0500

    Move pinouts to a header file

--- examples/floppy_capture_track_test/floppy_capture_track_test.ino
+++ examples/floppy_capture_track_test/floppy_capture_track_test.ino
@@ -1,48 +1,53 @@
 #include <Adafruit_Floppy.h>
 
-// If using SAMD51, turn on TINYUSB USB stack
 #if defined(ADAFRUIT_FEATHER_M4_EXPRESS)
-#define DENSITY_PIN  A0    // IDC 2
-#define INDEX_PIN    A1    // IDC 8
-#define SELECT_PIN   A2    // IDC 12
-#define MOTOR_PIN    A3    // IDC 16
-#define DIR_PIN      A4    // IDC 18
-#define STEP_PIN     A5    // IDC 20
-#define WRDATA_PIN   13    // IDC 22 (not used during read)
-#define WRGATE_PIN   12    // IDC 24 (not used during read)
-#define TRK0_PIN     11    // IDC 26
-#define PROT_PIN     10    // IDC 28
-#define READ_PIN      9    // IDC 30
-#define SIDE_PIN      6    // IDC 32
-#define READY_PIN     5    // IDC 34
-#elif defined (ARDUINO_ADAFRUIT_FEATHER_RP2040)
-#define DENSITY_PIN  A0    // IDC 2
-#define INDEX_PIN    A1    // IDC 8
-#define SELECT_PIN   A2    // IDC 12
-#define MOTOR_PIN    A3    // IDC 16
-#define DIR_PIN      24    // IDC 18
-#define STEP_PIN     25    // IDC 20
-#define WRDATA_PIN   13    // IDC 22 (not used during read)
-#define WRGATE_PIN   12    // IDC 24 (not used during read)
-#define TRK0_PIN     11    // IDC 26
-#define PROT_PIN     10    // IDC 28
-#define READ_PIN      9    // IDC 30
-#define SIDE_PIN      6    // IDC 32
-#define READY_PIN     5    // IDC 34
-#elif defined (ARDUINO_RASPBERRY_PI_PICO)
-#define DENSITY_PIN  2     // IDC 2
-#define INDEX_PIN    3     // IDC 8
-#define SELECT_PIN   4     // IDC 12
-#define MOTOR_PIN    5     // IDC 16
-#define DIR_PIN      6     // IDC 18
-#define STEP_PIN     7     // IDC 20
-#define WRDATA_PIN   8     // IDC 22 (not used during read)
-#define WRGATE_PIN   9     // IDC 24 (not used during read)
-#define TRK0_PIN    10     // IDC 26
-#define PROT_PIN    11     // IDC 28
-#define READ_PIN    12     // IDC 30
-#define SIDE_PIN    13     // IDC 32
-#define READY_PIN   14     // IDC 34
+#define DENSITY_PIN A1 // IDC 2
+#define INDEX_PIN A5   // IDC 8
+#define SELECT_PIN A0  // IDC 12
+#define MOTOR_PIN A2   // IDC 16
+#define DIR_PIN A3     // IDC 18
+#define STEP_PIN A4    // IDC 20
+#define WRDATA_PIN 13  // IDC 22
+#define WRGATE_PIN 12  // IDC 24
+#define TRK0_PIN 10    // IDC 26
+#define PROT_PIN 11    // IDC 28
+#define READ_PIN 9     // IDC 30
+#define SIDE_PIN 6     // IDC 32
+#define READY_PIN 5    // IDC 34
+#elif defined(ARDUINO_ADAFRUIT_FEATHER_RP2040)
+#define DENSITY_PIN A1 // IDC 2
+#define INDEX_PIN 25   // IDC 8
+#define SELECT_PIN A0  // IDC 12
+#define MOTOR_PIN A2   // IDC 16
+#define DIR_PIN A3     // IDC 18
+#define STEP_PIN 24    // IDC 20
+#define WRDATA_PIN 13  // IDC 22
+#define WRGATE_PIN 12  // IDC 24
+#define TRK0_PIN 10    // IDC 26
+#define PROT_PIN 11    // IDC 28
+#define READ_PIN 9     // IDC 30
+#define SIDE_PIN 8     // IDC 32
+#define READY_PIN 7    // IDC 34
+#ifndef USE_TINYUSB
+#error "Please set Adafruit TinyUSB under Tools > USB Stack"
+#endif
+#elif defined(ARDUINO_RASPBERRY_PI_PICO)
+#define DENSITY_PIN 2 // IDC 2
+#define INDEX_PIN 3   // IDC 8
+#define SELECT_PIN 4  // IDC 12
+#define MOTOR_PIN 5   // IDC 16
+#define DIR_PIN 6     // IDC 18
+#define STEP_PIN 7    // IDC 20
+#define WRDATA_PIN 8  // IDC 22 (not used during read)
+#define WRGATE_PIN 9  // IDC 24 (not used during read)
+#define TRK0_PIN 10   // IDC 26
+#define PROT_PIN 11   // IDC 28
+#define READ_PIN 12   // IDC 30
+#define SIDE_PIN 13   // IDC 32
+#define READY_PIN 14  // IDC 34
+#ifndef USE_TINYUSB
+#error "Please set Adafruit TinyUSB under Tools > USB Stack"
+#endif
 #else
 #error "Please set up pin definitions!"
 #endif
